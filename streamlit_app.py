fe48d649c76f6b9aa7567ab77115a2f007d47268
juanmanfranco <juan.manny.franco@gmail.com>
2026-09-02 08:24:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
+
 import os
 import pandas as pd
 from snowflake.snowpark.functions import col, when_matched
@@ -25,8 +26,8 @@ st.write(
 name_on_order = st.text_input ('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ',name_on_order)
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
